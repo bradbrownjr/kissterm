4ae8425d359dde2547c681fd15a2808c6f9e1c55
bradbrownjr <bradbrownjr@gmail.com>
2026-09-23 01:12:07 +0000

    Regenerate screenshots for the F1 Help tab in the tab row

--- kissterm/__init__.py
+++ kissterm/__init__.py
@@ -14,7 +14,7 @@ import __version__` fails with a confusing "unknown location" ImportError --
 this has already happened once.
 """
 
-__version__ = "0.1.200"
+__version__ = "0.1.201"
 
 __all__ = ["__version__"]
 
